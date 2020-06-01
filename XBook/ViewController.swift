6d1fc12a92fd46b70fc96ca1c8db4cdf84b0f70d
Ganang <ganang.arief.p@gmail.com>
2020-06-01 17:37:27 +0700

    Initial Commit

---
+++ XBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XBook
+//
+//  Created by Ganang Arief Pratama on 01/06/20.
+//  Copyright © 2020 Infinity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
